e94b6faf9243eefa9699e906df6aab0e3a343d1f
sgallon <shcmsgallon@outlook.com>
2021-06-15 17:13:12 +0800

    add middleware

--- backend/api.py
+++ backend/api.py
@@ -8,20 +8,37 @@
 # to run:
 # $ uvicorn api:app --reload
 
+import uvicorn
 from datetime import datetime
 from dateutil import parser
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from logger_utils import logger
 from mongo_utils.mongo_client_utils import read_with_condition_from
 from config import MONGO_URI, MONGO_DB_NAME, MONGO_COLLECTION_NAME
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:8080",
+    "https://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World"}
 
 
 # @app.get("/items/{item_id}")
@@ -57,3 +74,24 @@ async def read_pollution_data_place(lat: float, long: float, startdate: Optional
     records = read_with_condition_from(MONGO_URI, MONGO_DB_NAME, MONGO_COLLECTION_NAME,
                                        query_dict=query_dict, projection_dict={"_id": 0})
     return {"result_len": len(records), "result": records}
+
+
+@app.get("/test_api")
+async def read_test_api():
+    return {"time": {"updated": "Jun 7, 2021 08:54:00 UTC", "updatedISO": "2021-06-07T08:54:00+00:00",
+                     "updateduk": "Jun 7, 2021 at 09:54 BST"},
+            "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data "
+                          "converted using hourly conversion rate from openexchangerates.org",
+            "chartName": "Bitcoin",
+            "bpi": {
+                "USD": {"code": "USD", "symbol": "&#36;", "rate": "36,129.2477", "description": "United States Dollar",
+                        "rate_float": 36129.2477},
+                "GBP": {"code": "GBP", "symbol": "&pound;", "rate": "25,560.5756",
+                        "description": "British Pound Sterling",
+                        "rate_float": 25560.5756},
+                "EUR": {"code": "EUR", "symbol": "&euro;", "rate": "29,692.8584", "description": "Euro",
+                        "rate_float": 29692.8584}}}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app='api:app', host="localhost", port=8000, reload=True, debug=True)
